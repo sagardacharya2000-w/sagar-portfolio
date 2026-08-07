53c27010bde0c6779e4c7ec969cb86917016d2ce
Sagar Acharya <sagaracharyad2000@gmail.com>
2026-08-07 16:23:14 +0545

    initial protfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import {nav} from 'framer-motion/client'
+import React, {useState} from 'react'
+import { FaXmark } from 'react-icons/fa6'
+import { FaBars } from 'react-icons/fa6'
+
+const navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+    
+  return (
+    <nav className="fixed w-full z-50 bg-dark-100/90 backdrop-blur-sm py-4 px-8 shadow-lg"> 
+      <div className='container mx-auto flex justify-between items-center'>
+        <div>
+            <a href="#" className="text-3xl font-bold text-white">
+                Sagar
+                <span className="text-purple">Acharya</span>
+                <div className="w-4 h-4 bg-purple rounded-full"></div>
+            </a>
+        </div>
+         <div className="hidden md:flex space-x-10">
+            <a href="#home" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>Home</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a href="#about" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>About</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a href="#skills" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>Skills</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a href="#projects" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>Projects</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a href="#experience" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>Experience</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+            <a href="#contact" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                <span>Contact</span>
+                <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full"></span>
+            </a>
+
+         </div>
+
+         {/* mobile button */}
+         <div className='md:hidden'>
+            {
+                showMenu ?
+                 <FaBars onClick={()=>setShowMenu(!showMenu)}
+                  className="text-2xl cursor-pointer"/>:
+                 <FaXmark onClick={()=>setShowMenu(!showMenu)}
+                  className="text-2xl cursor-pointer"/>
+            }
+           
+            
+
+         </div>
+
+          
+      </div>
+
+
+    </nav>
+  )
+}
+
+export default navbar
